21699aa3517f88361ab8e6cd31522f09b0957b48
wfriedland <wwfriedland@yahoo.com>
2015-11-08 06:03:40 -0800

    New project to develop rest interfaces from asp.net

---
+++ restBing/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace restBing.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        regVersion registryV = new regVersion();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Versions from Registry:";
+            ViewData["vers"] = registryV.GetVersionFromRegistry();
+            var verList = ViewData["vers"];
+            Console.WriteLine(verList.ToString());
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
